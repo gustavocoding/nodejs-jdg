5e0982376076db1f13849a35cfaf6216cda03f20
gustavonalle <gustavonalle@gmail.com>
2016-05-26 21:39:53 +0100

    Reuse client connection and print only the first visit

--- api/controllers/greeter.js
+++ api/controllers/greeter.js
@@ -2,6 +2,11 @@
 
 var infinispan = require('infinispan');
 
+var jdgHost = process.env.DATAGRID_HOTROD_SERVICE_HOST || "127.0.0.1";
+var jdgPort = process.env.DATAGRID_HOTROD_SERVICE_PORT || 11222;
+
+var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
+
 var util = require('util');
 module.exports = {
   greet : greet 
@@ -9,19 +14,17 @@ module.exports = {
 
 function greet(req, res) {
 
-  var name = req.swagger.params.name.value || 'stranger';
+  var name = req.swagger.params.name.value;
 
-  var connected = infinispan.client({port: 11222, host: '127.0.0.1'}, {version: '2.2'});
-  var resp; 
   connected.then(function (client) {
     client.get(name).then(
           function(value) { 
              if(value == undefined)  {
                   res.json(util.format('Nice to meet you %s!', name))
+                  client.put(name, new Date().toLocaleString())
              } else {
-                  res.json(util.format('Welcome back %s! Your last time here was on %s', name, value))
+                  res.json(util.format('Welcome back %s! Your first visit here was on %s', name, value))
              }
-             client.put(name, new Date().toLocaleString())
           })
    })
 }
